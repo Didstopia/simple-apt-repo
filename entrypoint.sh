8ee9ba6b9eee1f181b5be76508d30933322f54fb
Pauli Jokela <pauli.jokela@didstopia.com>
2023-03-22 08:49:33 +0200

    Fixing CI

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,6 +25,22 @@ if ! id -u "${PUID}" > /dev/null 2>&1; then
   useradd -u "${PUID}" -g "${PGID}" -o -m "${USER}"
 fi
 
+# If GITHUB_WORKSPACE is set, then we should modify the folder environment variables
+# so that they're relative to the GITHUB_WORKSPACE.
+if [ -n "${GITHUB_WORKSPACE}" ]; then
+  # Verify that REPO_USE_RELATIVE is set to "true", otherwise skip this step
+  if [ "${REPO_USE_RELATIVE}" == "true" ]; then
+    echo "Detected GITHUB_WORKSPACE set to ${GITHUB_WORKSPACE}, adjusting repository root to be relative to the workspace path ..."
+    export REPO_DIR="${GITHUB_WORKSPACE}/${REPO_DIR}"
+    export REPO_PACKAGES_DIR="${GITHUB_WORKSPACE}/${REPO_PACKAGES_DIR}"
+    export REPO_KEYS_DIR="${GITHUB_WORKSPACE}/${REPO_KEYS_DIR}"
+  else
+    echo "Detected GITHUB_WORKSPACE set to ${GITHUB_WORKSPACE}, but REPO_USE_RELATIVE is not set to \"true\", skipping relative path adjustments ..."
+  fi
+else
+  echo "GITHUB_WORKSPACE is not set, skipping relative path adjustments ..."
+fi
+
 # Ensure that the user with PUID and PGID owns all of the appropriate directories
 chown -R "${PUID}":"${PGID}" "${REPO_DIR}" "${REPO_PACKAGES_DIR}" "${REPO_KEYS_DIR}"
 
